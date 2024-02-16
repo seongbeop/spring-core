9c633b1a9a4b62849071b0821db0f693b9508276
seongbeop <seongbeop0@gmail.com>
2024-02-16 10:58:24 +0900

    spring core Spring bean 조회하기

---
+++ src/test/java/hello/core/beanFind/ApplicationContextInfoTest.java
@@ -0,0 +1,42 @@
+package hello.core.beanFind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 bean 출력")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("Name = " + beanDefinitionName + " object = " + bean) ;
+
+        }
+
+    }
+
+    @Test
+    @DisplayName("애플리케이션 bean 출력")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            //
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("Name = " + beanDefinitionName + " object = " + bean) ;
+            }
+        }
+    }
+}
+
+
